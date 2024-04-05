cb869b19df64ddf773df0492fc78a858f0e2183e
yepi15 <yepi.graffer@gmail.com>
2024-04-05 13:40:25 -0500

    SmarhM365

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -6,7 +6,7 @@
 // speed limits for invividual modes in kph
 #define SPEEDLIMIT_ECO 6
 #define SPEEDLIMIT_NORMAL 20
-#define SPEEDLIMIT_SPORT 50
+#define SPEEDLIMIT_SPORT 35
 
 // motor current limits for invividual modes in mA
 // note that hacked firmware allows up to 55amps motor phase current
